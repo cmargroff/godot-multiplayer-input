5f9ba2f9942cd56c728c897a1254cd8fb2eeb795
Chris Margroff <sora3087@gmail.com>
2026-09-16 00:42:54 -0400

    create test controller UIs with each attached controller

--- addons/multiplayer_input/test/Test.cs
+++ addons/multiplayer_input/test/Test.cs
@@ -1,14 +1,36 @@
+using System;
 using Godot;
 
 namespace GodotMultiplayerInput;
 
-public partial class Test : Control
+public partial class Test : GridContainer
 {
   [Export] private PackedScene TestControllerScene;
   private InputManager inputManager = new InputManager();
   public override void _Ready()
   {
     inputManager.ParseMap("res://addons/multiplayer_input/test/inputmap.cfg");
+    Input.JoyConnectionChanged += OnJoyConnectionChanged;
+  }
+  private void OnJoyConnectionChanged(long deviceId, bool connected)
+  {
+    if (connected)
+    {
+      if (TestControllerScene != null)
+      {
+        var controller = TestControllerScene.Instantiate<TestController>();
+        controller.PlayerInput = inputManager.GetPlayer((int)deviceId);
+        AddChild(controller);
+        var childCount = GetChildren().Count;
+        if (childCount < 4)
+        {
+          Columns = childCount;
+        }
+        else
+        {
+          Columns = (int)Math.Ceiling(Math.Sqrt(childCount));
+        }
+      }
     }
   }
 }
