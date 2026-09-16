21627162e047eee064c9241338040314d2c4c3f5
Chris Margroff <sora3087@gmail.com>
2026-09-16 11:23:11 -0400

    update test scene to use input connection events

--- addons/multiplayer_input/test/Test.cs
+++ addons/multiplayer_input/test/Test.cs
@@ -8,37 +8,49 @@ public partial class Test : GridContainer
 {
   [Export] private PackedScene TestControllerScene;
   private InputManager inputManager = new InputManager();
-  private List<int> playerIds = new List<int>();
+  private Dictionary<int, Control> playerIds = new Dictionary<int, Control>();
+
   public override void _Ready()
   {
     inputManager.ParseMap("res://addons/multiplayer_input/test/inputmap.cfg");
-    Input.JoyConnectionChanged += OnJoyConnectionChanged;
+    inputManager.PlayerConnected += PlayerConnected;
+    inputManager.PlayerRemoved += PlayerRemoved;
   }
-  private void OnJoyConnectionChanged(long deviceId, bool connected)
+  private void PlayerConnected(PlayerInput playerInput)
   {
-    if (connected)
+    if (TestControllerScene != null)
     {
-      if (TestControllerScene != null)
+      if (playerIds.ContainsKey(playerInput.PlayerId))
       {
-        if (playerIds.Contains((int)deviceId))
-        {
-          return;
-        }
-        var controller = TestControllerScene.Instantiate<TestController>();
-        var playerInput = inputManager.GetPlayer((int)deviceId);
-        playerIds.Add((int)deviceId);
-        controller.PlayerInput = playerInput;
-        AddChild(controller);
-        var childCount = GetChildren().Count;
-        if (childCount < 4)
-        {
-          Columns = childCount;
-        }
-        else
-        {
-          Columns = (int)Math.Ceiling(Math.Sqrt(childCount));
-        }
+        return;
       }
+      var controller = TestControllerScene.Instantiate<TestController>();
+      playerIds[playerInput.PlayerId] = controller;
+      controller.PlayerInput = playerInput;
+      AddChild(controller);
+      UpdateColumns();
+    }
+  }
+  private void PlayerRemoved(int playerId)
+  {
+    if (playerIds.ContainsKey(playerId))
+    {
+      var controller = playerIds[playerId];
+      playerIds.Remove(playerId);
+      controller.QueueFree();
+      UpdateColumns();
+    }
+  }
+  private void UpdateColumns()
+  {
+    var childCount = GetChildren().Count;
+    if (childCount < 4)
+    {
+      Columns = childCount;
+    }
+    else
+    {
+      Columns = (int)Math.Ceiling(Math.Sqrt(childCount));
     }
   }
 }
