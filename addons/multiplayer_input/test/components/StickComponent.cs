d6a9acdd16e9795eda16ba82ef12d180bb6f5d87
Chris Margroff <sora3087@gmail.com>
2026-09-15 21:39:30 -0400

    update components

--- addons/multiplayer_input/test/components/StickComponent.cs
+++ addons/multiplayer_input/test/components/StickComponent.cs
@@ -1,18 +1,20 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class StickComponent : Control
-{    
+{
+    [Export] private TextureRect _clear;
     [Export] private TextureRect _fill;
+    [Export] public float DistanceScale = 15.0f;
     public Vector2 Value
     {
-        get => Value;
+        get => Position / DistanceScale;
         set
         {
             var modulate = _fill.Modulate;
             modulate.A = value.Length();
             _fill.Modulate = modulate;
-            Position = value;
+            _clear.OffsetTransformPosition = value * DistanceScale;
         }
     }
 
